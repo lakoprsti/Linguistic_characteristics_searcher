655673f1fd663b9fb3a33049061588989b723ba2
juliapakai <julia.pakai@gmail.com>
2022-07-19 20:52:36 +0100

    SQL database with phoneme chart, bridge table and languages&phonemes table

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE phonlang;
 USE phonlang;
 
+-- create phoneme feature chart as table
 CREATE TABLE phonemechart (
   primary_key VARCHAR(4) NOT NULL PRIMARY KEY,
   phoneme     VARCHAR(5) NOT NULL,
@@ -90,7 +91,7 @@ VALUES
 ('r2','ʁ',TRUE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,NULL,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
 ('h2','ħ',TRUE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,FALSE,NULL,NULL,NULL,NULL,TRUE,NULL,FALSE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
 ('gl1','ʕ',TRUE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,FALSE,NULL,NULL,NULL,NULL,TRUE,NULL,FALSE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
-('h3','h',FALSE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,FALSE,NULL,NULL,NULL,NULL,FALSE,NULL,FALSE,TRUE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
+('h','h',FALSE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,FALSE,NULL,NULL,NULL,NULL,FALSE,NULL,FALSE,TRUE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
 ('h4','ɦ',FALSE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,FALSE,NULL,NULL,NULL,NULL,FALSE,NULL,TRUE,TRUE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
 ('gl2','ʔ',FALSE,FALSE,FALSE,FALSE,NULL,FALSE,NULL,NULL,FALSE,NULL,NULL,NULL,NULL,FALSE,NULL,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE,FALSE),
 ('tsh','t͡ʃ',TRUE,FALSE,FALSE,FALSE,NULL,TRUE,FALSE,TRUE,TRUE,TRUE,FALSE,FALSE,FALSE,FALSE,NULL,FALSE,FALSE,FALSE,NULL,TRUE,FALSE,TRUE,FALSE),
@@ -114,3 +115,127 @@ VALUES
 ('w','w',FALSE,TRUE,FALSE,FALSE,TRUE,FALSE,NULL,NULL,TRUE,TRUE,FALSE,TRUE,FALSE,FALSE,NULL,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
 ('w2','ɥ',FALSE,TRUE,FALSE,FALSE,TRUE,FALSE,NULL,NULL,TRUE,TRUE,FALSE,FALSE,FALSE,FALSE,NULL,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE),
 ('w3','ɰ',FALSE,TRUE,FALSE,FALSE,FALSE,FALSE,NULL,NULL,TRUE,TRUE,FALSE,TRUE,FALSE,FALSE,NULL,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE);
+
+-- bridge table with languages
+CREATE TABLE languages(
+   primary_key VARCHAR(5) NOT NULL PRIMARY KEY,
+   language VARCHAR(20) NOT NULL
+);
+
+INSERT INTO
+languages(primary_key, language)
+VALUES
+('en','English'),
+('hu','Hungarian'),
+('sp','Spanish');
+
+
+-- table with Languages and the phonemes they contain
+
+CREATE TABLE langphons(
+   id INT(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   language VARCHAR(5) NOT NULL,
+   phoneme  VARCHAR(5) NOT NULL,
+   FOREIGN KEY (language) REFERENCES languages(primary_key),
+   FOREIGN KEY (phoneme) REFERENCES phonemechart(primary_key)
+);
+
+INSERT INTO
+langphons(language, phoneme)
+VALUES
+('en','a1'),
+('en','a2'),
+('en','a4'),
+('en','a5'),
+('en','o1'),
+('en','o2'),
+('en','o3'),
+('en','e1'),
+('en','e2'),
+('en','e5'),
+('en','e7'),
+('en','u2'),
+('en','u3'),
+('en','i2'),
+('en','i4'),
+('en','t'),
+('en','d'),
+('en','s'),
+('en','z'),
+('en','thvl'),
+('en','thv'),
+('en','h'),
+('en','sh'),
+('en','zh'),
+('en','p'),
+('en','b'),
+('en','f'),
+('en','v'),
+('en','k'),
+('en','g'),
+('en','tsh'),
+('en','dzh'),
+('en','m'),
+('en','n'),
+('en','n2'),
+('en','l'),
+('en','r'),
+('en','j'),
+('en','w'),
+('hu','i2'),
+('hu','u2'),
+('hu','u5'),
+('hu','o3'),
+('hu','a1'),
+('hu','a4'),
+('hu','e2'),
+('hu','e5'),
+('hu','t'),
+('hu','d'),
+('hu','s'),
+('hu','z'),
+('hu','h'),
+('hu','sh'),
+('hu','zh'),
+('hu','ty'),
+('hu','gy'),
+('hu','p'),
+('hu','b'),
+('hu','f'),
+('hu','v'),
+('hu','k'),
+('hu','g'),
+('hu','tsh'),
+('hu','dzh'),
+('hu','ts'),
+('hu','dz'),
+('hu','m'),
+('hu','n'),
+('hu','n4'),
+('hu','l'),
+('hu','r'),
+('hu','j'),
+('sp','i2'),
+('sp','u2'),
+('sp','o3'),
+('sp','a4'),
+('sp','e2'),
+('sp','t'),
+('sp','d'),
+('sp','s'),
+('sp','thvl'),
+('sp','sh'),
+('sp','gy2'),
+('sp','p'),
+('sp','b'),
+('sp','f'),
+('sp','k'),
+('sp','g'),
+('sp','kh'),
+('sp','tsh'),
+('sp','m'),
+('sp','n'),
+('sp','n4'),
+('sp','l'),
+('sp','l3'),
+('sp','r');
